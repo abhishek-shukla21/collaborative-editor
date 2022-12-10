936bc3ef3081e41c070e948624720d4a78838a3d
Abhishek Shukla <50260867+abhishek-shukla21@users.noreply.github.com>
2022-12-10 07:34:48 +0000

    initial commit

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('text_change')
+def handle_text_change(data):
+    text = data['text']
+    socketio.emit('update_text', {'text': text})
+
+if __name__ == '__main__':
+    socketio.run(app)
